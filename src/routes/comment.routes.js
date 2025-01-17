e257189c1ac0d9e70a7d83d13f72a17880671478
Abhinav-378 <abhichoukse05@gmail.com>
2025-01-17 22:36:14 +0530

    added routes for video comments

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { verifyJWT } from '../middlewares/auth.middleware';
+import { addComment, deleteComment, getVideoComments, updateComment } from '../controllers/comment.controller';
+const router = Router();
+
+router.use(verifyJWT); // Apply this middleware to all routes in this file
+
+router.route("/:videoId").get(getVideoComments).post(addComment); 
+
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+
+export default router;
