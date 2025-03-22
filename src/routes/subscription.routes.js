75ae08c248e973c3effe5d9ffd4e1e552671f065
Abhinav-378 <abhichoukse05@gmail.com>
2025-03-22 10:08:08 +0530

    updated subscription routes to use express router

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,28 +1,19 @@
-import mongoose, {isValidObjectId} from "mongoose"
- import {User} from "../models/user.model.js"
- import { Subscription } from "../models/subscription.model.js"
- import {ApiError} from "../utils/ApiError.js"
- import {ApiResponse} from "../utils/ApiResponse.js"
- import {asyncHandler} from "../utils/asyncHandler.js"
- 
- 
- const toggleSubscription = asyncHandler(async (req, res) => {
-     const {channelId} = req.params
-     // TODO: toggle subscription
- })
- 
- // controller to return subscriber list of a channel
- const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-     const {channelId} = req.params
- })
- 
- // controller to return channel list to which user has subscribed
- const getSubscribedChannels = asyncHandler(async (req, res) => {
-     const { subscriberId } = req.params
- })
- 
- export {
-     toggleSubscription,
+import { Router } from 'express';
+ import {
+     getSubscribedChannels,
      getUserChannelSubscribers,
-     getSubscribedChannels
- }
+     toggleSubscription,
+ } from "../controllers/subscription.controller.js"
+ import {verifyJWT} from "../middlewares/auth.middleware.js"
+ 
+ const router = Router();
+ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+ 
+ router
+     .route("/c/:channelId")
+     .get(getSubscribedChannels)
+     .post(toggleSubscription);
+ 
+ router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+ 
+ export default router
